4186a0bc853102097f13344b24aa081c06d96f74
Bosif <boannarutjes@gmail.com>
2024-09-13 22:15:57 +0200

    added styling

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -4,7 +4,7 @@ export default function WeatherForecast(props) {
   return (
     <div className="WeatherForecast">
       <div className="row">
-        <div className="col">Thu Icon 19 10</div>
+        <div className="col">Thu icon 19 10</div>
       </div>
     </div>
   );
