00b90c7f0888a855c6ac6197d8e07aa76470a887
Bosif <boannarutjes@gmail.com>
2024-09-15 18:23:10 +0200

    added the time

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -7,9 +7,8 @@ export default function WeatherInfo(props) {
     <div className="WeatherInfo">
       <h1>{props.data.city}</h1>
       <ul>
-        <li>
-          <FormattedDate date={props.data.date} />
-        </li>
+        <FormattedDate date={props.data.date} />
+        <li></li>
         <li className="text-capitalize">{props.data.description}</li>
       </ul>
       <div className="row mt-3">
