bb8bcb901cf0be05b3337623db0d33953c14856c
Bosif <boannarutjes@gmail.com>
2024-09-15 14:03:11 +0200

    added the forecast styling (1 day)

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,17 +1,17 @@
 import React from "react";
+import "./WeatherForecast.css";
 
 export default function WeatherForecast(props) {
   return (
     <div className="WeatherForecast">
       <div className="row">
         <div className="col">
-          Thu 19
-          <img
-            src={props.data.iconUrl}
-            alt={props.data.description}
-            className="float-left"
-          />
-          10
+          <div className="WeatherForecast-day">Thu</div>
+          <img src={props.data.iconUrl} alt={props.data.description} />
+          <div className="WeatherForecast-temperatures">
+            <span className="WeatherForecast-temperature-max">19°</span>
+            <span className="WeatherForecast-temperature-min">10°</span>
+          </div>
         </div>
       </div>
     </div>
